7f4a569d47022de931358169c86c1fb235c90752
Muhammed Ajmal <ajmalkunjammed@gmail.com>
2024-10-29 15:41:26 +0530

    New commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const flash = require('connect-flash')
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-const PORT = 3000 || process.env.PORT
+const PORT =process.env.PORT|| 3000  
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
