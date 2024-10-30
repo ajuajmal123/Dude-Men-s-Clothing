a7785d58582300a622a2d0fad3b1f47acc5eee07
Muhammed Ajmal <ajmalkunjammed@gmail.com>
2024-10-30 20:43:47 +0530

    google authentication  fixeing again

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ passport.use(new GoogleStrategy({
 
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://3000/google/callback',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
     passReqToCallback:true
 },
 async function(request, accessToken, refreshToken, profile, done) {
